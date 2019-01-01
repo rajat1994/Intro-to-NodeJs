f0fe0511c010490c173b622d541203011613cf85
rajat1994 <rajatarora21@gmail.com>
2019-01-01 22:16:36 +0530

    remove unwanted comments

--- account-service.js
+++ account-service.js
@@ -4,5 +4,4 @@ let savingsAccountBalance = 1000
 let retirementAccountBalance = 20000
 
 let totalBalance = sum([checkingAccountBalance,savingsAccountBalance,retirementAccountBalance])
-//let totalBalance = sum([11,12])
 console.log(totalBalance)
